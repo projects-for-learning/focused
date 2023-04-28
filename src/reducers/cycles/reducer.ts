7a5903faaeeba980e87b51c97a07addcb2efd99f
Henrique Pereira <henrique802011@gmail.com>
2023-04-27 22:16:18 -0300

    Add: Add use reducer to the project

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { ActionsType } from "./actions";
+
 export interface Cycle {
   id: string;
   task: string;
@@ -12,4 +14,40 @@ interface CycleState {
   activeCycleId: string | null;
 }
 
-export function cyclesReducers(state: CycleState, action: any) {}
+export function cyclesReducers(state: CycleState, action: any) {
+  switch (action.type) {
+    case ActionsType.ADD_NEW_CYCLE:
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.newCycle],
+        activeCycleId: action.payload.newCycle.id,
+      };
+    case ActionsType.INTERRUPT_CURRENT_CYCLE:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() };
+          } else {
+            return cycle;
+          }
+        }),
+        activeCycleId: null,
+      };
+    case ActionsType.MARK_CURRENT_CYCLE_FINISHED:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() };
+          } else {
+            return cycle;
+          }
+        }),
+        activeCycleId: null,
+      };
+
+    default:
+      return state;
+  }
+}
